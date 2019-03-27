16a51d5f706730855dbb9686247a7328a5ab2d62
Raman Nanda <raman148@gmail.com>
2019-03-26 23:17:55 -0400

    Updating Tests / Adding Fixtures
    ⧙⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯
    ⧙ 🌲  ⨳ master
    ⧙⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯

--- tests/index.js
+++ tests/index.js
@@ -4,59 +4,124 @@ var git = require('simple-git');
 var fs = require("fs-extra");
 var path = require('path');
 var del = require('del');
+var tmp = require('tmp');
 
+var exec = require('child_process').exec;
 
 describe('git', function() {
 
-	var dir = '/tmp/git-meta-repo';
-	var repo;
+  var tmpCommitFile;
+  var tmpEmptyFile;
+  var tmpMessageFile;
+  var tmpAmendFile;
 
-	beforeEach(function() {
-		fs.ensureDirSync(dir);
-		fs.writeJsonSync(dir + '/test.json', {
-			'test': true
-		});
-		repo = git(dir).init();
-	});
+  beforeEach(function(done) {
+    tmpCommitFile = tmp.fileSync();
+    tmpEmptyFile = tmp.fileSync();
+    tmpMessageFile = tmp.fileSync();
+    tmpAmendFile = tmp.fileSync();
 
-	afterEach(function() {
-		del.sync([dir, dir + '/**'], {force: true});
-	});
+    fs.copySync('./tests/fixtures/1.txt', tmpCommitFile.name);
+    fs.copySync('./tests/fixtures/Empty.txt', tmpEmptyFile.name);
+    fs.copySync('./tests/fixtures/Message.txt', tmpMessageFile.name);
+    fs.copySync('./tests/fixtures/Amend.txt', tmpAmendFile.name);
+    done();
+  });
 
-  describe('git commit', function() {
-    it('should add commit meta if message is present', function(done) {
-      repo
-      	.add('./*')
-      	.commit("Initial Commit");
-      repo.log(function(err, results) {
-      	assert.equal(results.latest.message, "Initial Commit");
-      	done();
+  afterEach(function(done) {
+    fs.removeSync(tmpCommitFile.name);
+    fs.removeSync(tmpEmptyFile.name);
+    fs.removeSync(tmpMessageFile.name);
+    fs.removeSync(tmpAmendFile.name);
+
+    done();
+  });
+
+  describe('prepare-commit-msg', function() {
+    it('should do nothing if no arguments are passed', function(done) {
+      var cmd = './prepare-commit-msg';
+      exec(cmd, function(err, stdout, stderr) {
+        assert.equal(err, null);
+        assert.equal(stdout, "No arguments supplied!\n");
+        assert.equal(stderr, "");
+
+        assert.equal(
+          fs.readFileSync(tmpCommitFile.name, "utf8"),
+          fs.readFileSync('./tests/fixtures/1.txt', "utf8")
+        );
+        done();
       });
     });
-    it('should cancel commit if no message is present', function(done) {
-    	repo
-      	.add('./*')
-      	.commit("", function(err, commit) {
-      		assert.equal(err, 'Aborting commit due to empty commit message.\n');
-      		done();
-      	});
+
+    it('should add metadata on regular commit', function(done) {
+      var cmd = './prepare-commit-msg ' + tmpCommitFile.name;
+      exec(cmd, function(err, stdout, stderr) {
+        assert.equal(err, null);
+        assert.equal(stdout, "");
+        assert.equal(stderr, "");
+
+        assert.equal(
+          fs.readFileSync(tmpCommitFile.name, "utf8"),
+          fs.readFileSync('./tests/fixtures/1-Meta.txt', "utf8")
+        );
+        done();
+      });
+    });
+
+    it('should add metadata when message is passed (-m) - empty', function(done) {
+      var cmd = './prepare-commit-msg ' + tmpEmptyFile.name + ' message';
+      exec(cmd, function(err, stdout, stderr) {
+        assert.equal(err, null);
+        assert.equal(stdout, "");
+        assert.equal(stderr, "");
+
+        assert.equal(
+          fs.readFileSync(tmpEmptyFile.name, "utf8"),
+          fs.readFileSync('./tests/fixtures/1-Empty.txt', "utf8")
+        );
+        done();
+      });
+    });
+
+    it('should add metadata when message is passed (-m) - message', function(done) {
+      var cmd = './prepare-commit-msg ' + tmpMessageFile.name + ' message';
+      exec(cmd, function(err, stdout, stderr) {
+        assert.equal(err, null);
+        assert.equal(stdout, "");
+        assert.equal(stderr, "");
+
+        assert.equal(
+          fs.readFileSync(tmpMessageFile.name, "utf8"),
+          fs.readFileSync('./tests/fixtures/1-Message.txt', "utf8")
+        );
+        done();
+      });
+    });
+
+    it('should recomment metadata on commit ammend (--amend) - commit', function(done) {
+      var cmd = './prepare-commit-msg ' + tmpAmendFile.name + ' commit HASH';
+      exec(cmd, function(err, stdout, stderr) {
+        assert.equal(err, null);
+        assert.equal(stdout, "");
+        assert.equal(stderr, "");
+
+        assert.equal(
+          fs.readFileSync(tmpAmendFile.name, "utf8"),
+          fs.readFileSync('./tests/fixtures/1-Amend.txt', "utf8")
+        );
+        done();
+      });
+    });
+
+    it('should do nothing on merge', function(done) {
+      done();
     });
   });
 
-  describe('git commit -m', function() {
-    it('should add commit meta if message is present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+  describe('commit-msg', function() {
+    it('should exit 1 if message is empty', function() {
     });
-    it('should cancel commit if no message is present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-  describe('git commit --amend', function() {
-    it('should not update meta if ammending commit', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-    it('should cancel commit if commit message is not updated', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+    it('should remove commits from meta', function() {
     });
   });
 });
